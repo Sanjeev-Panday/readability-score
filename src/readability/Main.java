d1c8f552f7e5312de7f44a0c9cd04024eb37c3ab
Sanjeev Panday <sanjeev.pandey84@gmail.com>
2020-08-01 22:33:06 +0530

    Readability score code refactoring

--- src/readability/Main.java
+++ src/readability/Main.java
@@ -3,18 +3,30 @@ package readability;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.regex.Pattern;
 
+/**
+ * Meta data class to store meta data of the input text
+ */
+class MetaData {
+    public int sentences;
+    public int words;
+    public int characters;
+    public int syllables;
+    public int polysyllables;
+}
+
+/**
+ * Main class
+ */
 public class Main {
-    private static final String SENT_SPLIT_REGX = "\\.|!|\\?";
+    // Regular exp to calculate sentence count
+    private static final String SENT_SPLIT_REGX = "[.!?]";
+    // Regular exp to calculate word count
     private static final String WORD_SPLIT_REGX = "\\s+";
+    // Map to store score to age mapping ( age upper bound )
     private static final Map<Integer, Integer> map = new HashMap<>();
-    private static final double FIRST_COEF = 4.71D;
-    private static final double SECOND_COEF = 0.5D;
-    private static final double THIRD_COEF = 21.43d;
-    private static final Pattern pattern = Pattern.compile("[aeiouyAEIOUY]", Pattern.CASE_INSENSITIVE);
+    // Vowels
     private static final List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u', 'y');
-
     static {
         map.put(1, 6);
         map.put(2, 7);
@@ -32,95 +44,59 @@ public class Main {
         map.put(14, 24);
     }
 
-    private static int polysyllables;
-
+    /**
+     *
+     * @param args input argument array
+     * @throws FileNotFoundException File not found exception
+     */
     public static void main(String[] args) throws FileNotFoundException {
+
         Scanner sc = new Scanner(new File(args[0]));
+        MetaData metaData = getMetaData(sc.nextLine());
 
-        String inputString = sc.nextLine();
-        String[] sentences = inputString.split(SENT_SPLIT_REGX);
-        int numOfSent = sentences.length;
-        int noOfWords = 0;
-        int noOfChars = 0;
-        int syllableCount = 0;
-        int polysyllables = 0;
-        for (String sentence : sentences) {
-            String[] words = sentence.trim().split(WORD_SPLIT_REGX);
-            noOfWords += words.length;
-            for (String word : words) {
-                int count = countSyllables(word.toLowerCase());
-                syllableCount += count;
-                if (count > 2) {
-                    polysyllables++;
-                }
-            }
+        System.out.println("Words: " + metaData.words);
+        System.out.println("Sentences: " + metaData.sentences);
+        System.out.println("Characters: " + metaData.characters);
+        System.out.println("Syllables: " + metaData.syllables);
+        System.out.println("Polysyllables: " + metaData.polysyllables);
 
-        }
-        noOfChars += Arrays.stream(inputString.split("\\s+")).mapToInt(x -> x.length()).reduce(0, (sum, x) -> sum + x);
+        double ari = automaticReadabilityScore(metaData);
+        int ariAge = getAge(ari);
 
-        double score = FIRST_COEF * (noOfChars * 1.0 / noOfWords) + SECOND_COEF * (noOfWords * 1.0 / numOfSent) - THIRD_COEF;
+        double fk = fleshKincaidScore(metaData);
+        int fkAge  = getAge(fk);
 
-        System.out.println("Words: " + noOfWords);
-        System.out.println("Sentences: " + numOfSent);
-        System.out.println("Characters: " + noOfChars);
-        System.out.println("Syllables: " + syllableCount);
-        System.out.println("Polysyllables: " + polysyllables);
-        int key;
-        double ari = Math.floor(score * 100) / 100.0 ;
-        key = (int) Math.round(ari);
-        if(key > 13) key = 14;
-        int ariAge  = map.get(key);
+        double smog = simpleMeasureOfGobbledygook(metaData);
+        int smogAge  = getAge(smog);
 
-        double fk = fleshKincaidScore(numOfSent, noOfWords, syllableCount);
-        key = (int) Math.round(fk);
-        if(key > 13) key = 14;
-        int fkAge  = map.get(key);
-
-        double smog = simpleMeasureOfGobbledygook(numOfSent, polysyllables);
-        key = (int)Math.round(smog);
-        if(key > 13) key = 14;
-        int smogAge  = map.get(key);
-
-        double cl = colemanLiauIndex((noOfChars * 100.0 / noOfWords), (numOfSent * 100.0) / noOfWords);
-        key = (int) Math.round(cl);
-        if(key > 13) key = 14;
-        int clAge  = map.get(key);
+        double cl = colemanLiauIndex(metaData);
+        int clAge  = getAge(cl);
 
         double avgAge = (fkAge + ariAge + smogAge + clAge) * 1.0 / 4;
 
-        System.out.printf("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+        System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
         Scanner scForInput = new Scanner(System.in);
         String input = scForInput.next();
         System.out.println();
+
         switch (input) {
             case "ARI":
-
                 System.out.println("Automated Readability Index: " + ari + " (about " + ariAge + " year olds)");
-
                 break;
             case "FK":
-
                 System.out.println("Flesch–Kincaid readability tests: " + fk + " (about " + fkAge + " year olds).");
-
                 break;
             case "SMOG":
-                score = simpleMeasureOfGobbledygook(numOfSent, polysyllables);
-                key = (int) Math.ceil(score);
                 System.out.println("Simple Measure of Gobbledygook: " + smog + " (about " + smogAge + " year olds).");
                 break;
             case "CL":
                 System.out.println("Coleman–Liau index: " + cl + " (about " + clAge + " year olds).");
                 break;
             case "all":
-
                 System.out.println("Automated Readability Index: " + ari + " (about " + ariAge + " year olds).");
-
                 System.out.println("Flesch–Kincaid readability tests: " + fk + " (about " + fkAge + " year olds).");
-
                 System.out.println("Simple Measure of Gobbledygook: " + smog + " (about " + smogAge + " year olds).");
-
                 System.out.println("Coleman–Liau index: " + cl + " (about " + clAge + " year olds).");
-
                 break;
         }
         System.out.println("This text should be understood in average by " + avgAge + " year olds.");
@@ -128,8 +104,44 @@ public class Main {
     }
 
     /**
-     * @param word
-     * @return
+     * Calculate count of sentences, words, characters, syllables and polysyllables
+     * present the input text.
+     * @param  input @link String
+     * @return MetaData
+     */
+    public static MetaData getMetaData(String input) {
+        MetaData metaData = new MetaData();
+        String[] sentArray = input.split(SENT_SPLIT_REGX);
+        metaData.sentences = sentArray.length;
+
+        for (String sentence : sentArray) {
+            String[] wordsArray = sentence.trim().split(WORD_SPLIT_REGX);
+            metaData.words += wordsArray.length;
+            for (String word : wordsArray) {
+                int count = countSyllables(word.toLowerCase());
+                metaData.syllables += count;
+                if (count > 2) {
+                    metaData.polysyllables++;
+                }
+            }
+        }
+        metaData.characters = metaData.characters + Arrays.stream(input.split("\\s+")).mapToInt(String::length).reduce(0, Integer::sum);
+        return metaData;
+    }
+
+    /**
+     * Gets the age for the score.
+     * @param score score of the text
+     * @return corresponding age
+     */
+    private static int getAge(double score) {
+        int key = (int) Math.round(score);
+        if(key > 13) key = 14;
+        return map.get(key);
+    }
+    /**
+     * @param word word to count syllables
+     * @return syllable count
      */
     private static int countSyllables(String word) {
         int counter = 0;
@@ -146,17 +158,26 @@ public class Main {
         return counter > 0 ? counter : 1;
     }
 
-    private static double fleshKincaidScore(int sents, int words, int syllables) {
-        double score = 0.39 * ((words * 1.0) / sents) + 11.8 * ((syllables * 1.0) / words) - 15.59;
+    private static double automaticReadabilityScore(MetaData metaData) {
+        double score = 4.71 * (metaData.characters * 1.0 / metaData.words)
+                + 0.5 * (metaData.words * 1.0 / metaData.sentences) - 21.43;
+        return Math.floor(score * 100) / 100.0 ;
+    }
+
+    private static double fleshKincaidScore(final MetaData metaData) {
+        double score = 0.39 * ((metaData.words * 1.0) / metaData.sentences)
+                + 11.8 * ((metaData.syllables * 1.0) / metaData.words) - 15.59;
         return Math.floor(score * 100) / 100.0;
     }
 
-    private static double simpleMeasureOfGobbledygook(int sentences, int polysyllables) {
-        double score = 1.043 * Math.sqrt(polysyllables * (30.0 / sentences)) + 3.1291;
+    private static double simpleMeasureOfGobbledygook(final MetaData metaData) {
+        double score = 1.043 * Math.sqrt(metaData.polysyllables * (30.0 / metaData.sentences)) + 3.1291;
         return Math.floor(score * 100) / 100.0;
     }
 
-    private static double colemanLiauIndex(double l, double s) {
+    private static double colemanLiauIndex(final MetaData metaData) {
+        double l = (metaData.characters * 100.0 / metaData.words);
+        double s = (metaData.sentences * 100.0) / metaData.words;
         double score = 0.0588 * l - 0.296 * s - 15.8;
         return Math.floor(score * 100) / 100.0;
     }
